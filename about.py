8ab8fbe3c08c4668201ecd524ba1921b98d404d3
Arthur Guilherme <104983447+devarthurguilherme@users.noreply.github.com>
2024-10-21 13:00:30 +0100

    About page

--- about.py
+++ about.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="Talk to Documents 📚",
+st.set_page_config(page_title="Arthur's Virtual Assistent 📚",
                    page_icon="📚", layout='wide')
 
-st.title("Talk to Documents 📚")
+st.title("About")
